21465cebfee9c132cbc9038fe3a621e9517b9ff5
gurum <gurumruthi.shruhri@gmail.com>
2022-08-16 22:55:46 +0200

    hello added with new printouts

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! change 4 again and again");
+        System.out.println("Hello world! change 5 again and again");
+
     }
 }
